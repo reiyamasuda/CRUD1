6003afc97c9204a8e9d10e7bbab612626b39fe03
reiyamasuda <masuda.reiya@gmail.com>
2021-02-06 15:12:11 +0900

    学習

---
+++ database/migrations/2021_02_06_054114_create_prefs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrefsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prefs', function (Blueprint $table) {
+            $table->increments('id')->unsigned()->comment('ID');
+            $table->string('name', 128)->comment('都道府県名');
+            $table->timestamp('created_at')->useCurrent()->comment('作成日時');
+            $table->timestamp('updated_at')->useCurrent()->comment('更新日時');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prefs');
+    }
+}
