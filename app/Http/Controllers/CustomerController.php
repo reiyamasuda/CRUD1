2c5ab4f77a1ec44691686aa0cbacaa2cedb22c5f
Satoshi Nagashiba <bobtabo.buhibuhi@gmail.com>
2021-03-14 18:28:14 +0900

    検索

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,10 +3,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     public function index() {
         return view('index');
     }
+
+    public function search(Request $request) {
+        return view('index');
+    }
+
+    //create
+
+
+    //detail
+
+    //edit
 }
